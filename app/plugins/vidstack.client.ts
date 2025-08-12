559b4f6ab04bf065c74bf81faa79a6a5ab553000
rony <ronssrony@gmail.com>
2025-08-12 15:26:53 +0600

    initialize Nuxt.js application with Vidstack integration and essential configurations

---
+++ app/plugins/vidstack.client.ts
@@ -0,0 +1,5 @@
+import 'vidstack/bundle';
+import { defineNuxtPlugin } from '#app';
+export default defineNuxtPlugin((nuxtApp) => {
+  // Register the vidstack plugin
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { vite as vidstack } from 'vidstack/plugins';
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag.startsWith('media-'),
+    },
+  },
+  vite: {
+    plugins: [vidstack()],
+  },
+})
